af6bc2faf83abaaca6cb9a69db97c8a2615d48f6
melloru <pythonubuntu12@gmail.com>
2024-10-17 19:56:59 -0100

    Реализовал регистрацию и логин, настроил навбар, исправил список пользователей. Добавил htmlы без реализации.

--- mysite/task_manager/forms.py
+++ mysite/task_manager/forms.py
@@ -1,49 +1,30 @@
 from django import forms
 from django.forms import EmailField
-
 from .models import Users
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 
-# class RegistrationForm(forms.Form):
-#     name = forms.CharField(max_length=150, label="Имя", widget=forms.TextInput(
-#         attrs={"class": "form-control", "placeholder": "Имя"}))
-#
-#     surname = forms.CharField(max_length=150, label="Фамилия", widget=forms.TextInput(
-#         attrs={"class": "form-control", "placeholder": "Фамилия"}))
-#
-#     nickname = forms.CharField(max_length=150, label="Имя пользователя", widget=forms.TextInput(
-#         attrs={"class": "form-control", "placeholder": "Имя пользователя"}),
-#                                help_text="Обязательное поле. Не более 150 символов. Только буквы, цифры и символы @/./+/-/_.")
-#
-#     password1 = forms.CharField(max_length=150, label="Пароль", widget=forms.TextInput(
-#         attrs={"class": "form-control", "placeholder": "Пароль"}))
-#
-#     password2 = forms.CharField(max_length=150, label="Подтверждение пароля", widget=forms.TextInput(
-#         attrs={"class": "form-control", "placeholder": "Подтверждение пароля"}))
 
-class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(
-        label="Имя",
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя"}))
-    last_name = forms.CharField(
-        label="Фамилия",
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Фамилия"}))
-    username = forms.CharField(
-        label="Имя пользователя",
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя пользователя"}))
+class RegistrationForm(forms.ModelForm):
     password1 = forms.CharField(
         label="Пароль",
-        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Пароль"}))
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Пароль"}),
+        help_text="Ваш пароль должен содержать как минимум 3 символа.")
     password2 = forms.CharField(
         label="Подтверждение пароля",
-        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Подтверждение пароля"}))
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Подтверждение пароля"}),
+        help_text="Для подтверждения введите, пожалуйста, пароль ещё раз.")
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "username", "password1", "password2")
-
+        fields = ["first_name", "last_name", "username", "password1", "password2"]
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Фамилия"}),
+            "username": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя пользователя"}),
+        }
 
     def clean_password1(self):
         password1 = self.cleaned_data.get("password1")
@@ -51,38 +32,20 @@ class RegistrationForm(UserCreationForm):
             raise ValidationError("Пароль должен содержать как минимум 3 символа.")
         return password1
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            raise ValidationError("Введенные пароли не совпадают.")
+        return password2
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()  # Сохраняем пользователя в БД
+        return user
 
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Пароли не совпадают.")
-
-
-# class RegistrationForm(forms.ModelForm):
-#
-#     password1 = forms.CharField(
-#         max_length=150,
-#         label="Пароль",
-#         widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Пароль"})
-#     )
-#
-#     password2 = forms.CharField(
-#         max_length=150,
-#         label="Подтверждение пароля",
-#         widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Подтверждение пароля"})
-#     )
-#
-#     class Meta:
-#         model = Users
-#         fields = ["name", "surname", "nickname"]
-#         widgets = {
-#             "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя"}),
-#             "surname": forms.TextInput(attrs={"class": "form-control", "placeholder": "Фамилия"}),
-#             "nickname": forms.TextInput(attrs={"class": "form-control", "placeholder": "Имя пользователя"}),
-#         }
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
--- mysite/task_manager/urls.py
+++ mysite/task_manager/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, all_users, user_login, registration
+from .views import index, all_users, user_login, registration, user_logout
 
 urlpatterns = [
     path("", index, name="index"),
     path("users/", all_users, name="all_users"),
     path("login/", user_login, name="login"),
     path("users/create/", registration, name="registration"),
+    path("logout/", user_logout, name="logout")
 ]
